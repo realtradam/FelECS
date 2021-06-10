26a23996164f3e056c440ac142c8eb71c892b501
realtradam <mr.malczewski@gmail.com>
2021-06-10 13:30:43 -0400

    cleanup

--- codeclimate/export-coverage.rb
+++ codeclimate/export-coverage.rb
@@ -4,12 +4,10 @@ require_relative 'env'
 class ReportCoverage
   class <<self
     def format
-      puts ENV['CC_TEST_REPORTER_ID']
       puts `./codeclimate/test-reporter-latest-linux-amd64 format-coverage -t simplecov`
     end
 
     def upload
-      puts ENV['CC_TEST_REPORTER_ID']
       puts `./codeclimate/test-reporter-latest-linux-amd64 upload-coverage --id #{ENV['CC_TEST_REPORTER_ID']}`
     end
   end
